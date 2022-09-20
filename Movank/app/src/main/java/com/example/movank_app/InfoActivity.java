63c86b3785f46d4e7b9106673a358ed6cc3f63be
edsonmier <edson_mier28@hotmail.com>
2022-09-19 22:38:28 -0500

    update: finish activities

--- Movank/app/src/main/java/com/example/movank_app/InfoActivity.java
+++ Movank/app/src/main/java/com/example/movank_app/InfoActivity.java
@@ -1,8 +1,13 @@
 package com.example.movank_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        LinearLayout backButton = (LinearLayout) findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(InfoActivity.this, MainActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(InfoActivity.this, (View) backButton, "transition2");
+                startActivity(intent, options.toBundle());
+            }
+        });
     }
+
+
 }
--- Movank/app/src/main/java/com/example/movank_app/MainActivity.java
+++ Movank/app/src/main/java/com/example/movank_app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.movank_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView initialImage = (ImageView) findViewById(R.id.imageView2);
+        initialImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(MainActivity.this, (View) initialImage, "transition1");
+                startActivity(intent, options.toBundle());
+            }
+        });
     }
+
 }
